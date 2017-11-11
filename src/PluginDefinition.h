1fd6a6999fdecdf668d7fefc0c7451c53ba1d388
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-11-11 14:46:00 -0800

    Add "p4 add" menu item and insert divider in menu

--- src/PluginDefinition.h
+++ src/PluginDefinition.h
@@ -44,7 +44,7 @@ const wchar_t NPP_PLUGIN_NAME[] = L"Npp4";
 //
 // Here define the number of your plugin commands
 //
-const int nbFunc = 5;
+const int nbFunc = 7;
 
 //
 // Initialization of your plugin data
@@ -88,5 +88,6 @@ std::wstring getCurrentFilePath();
 // p4 commands
 void checkoutOpenedFile();
 void revertOpenedFile();
+void addOpenedFile();
 
 #endif //PLUGINDEFINITION_H
